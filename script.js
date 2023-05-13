08ab953b7a122e6279bf7da3b07c5820d15ac757
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-05-13 02:06:29 -0400

    Add raindbow and darkening colors

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ function drawGrid(squaresPerSide) {
 	for (let index = 0; index < squaresPerSide**2; index++) {	
 		let square = document.createElement("div");
 		square.classList.add("square");
-		square.classList.remove("triggered")
 		square.style.height = `${squaresSidePixels}px`;
 		square.style.width = `${squaresSidePixels}px`;
 		square.style.fontSize = `${squaresSidePixels / 2.5}px`
-		// square.textContent = index + 1
 	
 		gridContainer.appendChild(square);
 	}
@@ -20,6 +18,13 @@ function drawGrid(squaresPerSide) {
 	squares.forEach(square => {
 		square.addEventListener("mouseenter", () => {
 			square.classList.add("triggered");
+			if (getComputedStyle(square).backgroundColor === "rgb(255, 255, 255)") {
+				square.style.backgroundColor = "#" + getRandomColor();
+			} else {
+				let firstColor = getHexColor(getComputedStyle(square).backgroundColor);
+				let newColor = LightenDarkenColor(firstColor, -10);
+				square.style.backgroundColor = "#" + newColor;
+			}
 		});
 	});
 }
@@ -54,3 +59,51 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+
+function getRandomColor() {
+	const letters = '0123456789ABCDEF';
+	let color = "";
+	for (let i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+  }
+
+  function getHexColor(color){
+	// H/T to StackOverflow user jave.web for this one.
+    //if color is already in hex, just return it...
+    if( color.toString().length == 6 ) return color;
+    
+    //leave only "R,G,B" :
+    color = color
+                .replace("rgba", "") //must go BEFORE rgb replace
+                .replace("rgb", "")
+				.replace("(", "")
+                .replace(")", "");
+    color = color.split(","); // get Array["R","G","B"]
+    
+    // 0) add leading #
+    // 1) add leading zero, so we get 0XY or 0X
+    // 2) append leading zero with parsed out int value of R/G/B
+    //    converted to HEX string representation
+    // 3) slice out 2 last chars (get last 2 chars) => 
+    //    => we get XY from 0XY and 0X stays the same
+    return  ""
+            + ( '0' + parseInt(color[0], 10).toString(16) ).slice(-2)
+            + ( '0' + parseInt(color[1], 10).toString(16) ).slice(-2)
+            + ( '0' + parseInt(color[2], 10).toString(16) ).slice(-2);
+}
+
+  function LightenDarkenColor(firstColor, percentChange) {
+	// h/t StackOverflow user Pimp Trizkit
+	let num = parseInt(firstColor, 16);
+	let r = (num >> 16) * (1 + (percentChange / 100));
+	let b = ((num >> 8) & 0x00FF) * (1 + (percentChange / 100));
+	let g = (num & 0x0000FF) * (1 + (percentChange / 100));
+	let newColor = g | (b << 8) | (r << 16);
+	newColor = newColor.toString(16);
+	while (newColor.length < 6) {
+		newColor = "0" + newColor;
+	}
+	return newColor;
+  }
